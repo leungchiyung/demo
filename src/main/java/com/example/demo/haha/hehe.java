737599ad170fbe06a3c91eb5c76e708fa4389c54
leungchiying <leungchiyung1112@gmail.com>
2022-09-18 12:02:52 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/haha/hehe.java
@@ -0,0 +1,15 @@
+package com.example.demo.haha;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hehe")
+
+public class hehe {
+    @RequestMapping("/hehe")
+    public String hehe() {
+        return "hehe!!!";
+    }
+}
