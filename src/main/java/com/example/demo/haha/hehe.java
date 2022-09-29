8f207814c3936d40823999e90dcec88a3a8ee457
leungchiying <leungchiyung1112@gmail.com>
2022-09-29 19:52:36 +0800

    ssss

--- src/main/java/com/example/demo/haha/hehe.java
+++ src/main/java/com/example/demo/haha/hehe.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class hehe {
     @RequestMapping("/hehe")
     public String hehe() {
-        return "heha!!!";
+        return "hehaaaa!!!";
     }
 }
